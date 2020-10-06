c35892439143e95c349260c415f59abb5f3ca6bf
Mario Zupan <mario@zupzup.org>
2020-10-06 11:59:02 +0200

    add stop tracking

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,15 @@ async fn main() -> Result<(), Error> {
     println!("fetching me and spaces...");
     let me = fetch_me(&token).await?;
     let spaces = fetch_spaces(&token).await?;
+    println!("fetched spaces: {:?} for {:?}", spaces, me);
 
     println!("fetching activities...");
     let activities = fetch_activities(&token).await?;
     if !activities.is_empty() {
         let tracking = start_tracking(&activities.get(0).expect("exists").id, &token).await?;
-        // TODO: stop tracking
+        print!("started tracking: {:?}", tracking);
+        let time_entry = stop_tracking(&token).await?;
+        print!("created time entry: {:?}", time_entry);
     }
     // TODO: create report and write file
 
@@ -91,12 +94,28 @@ async fn start_tracking(activity_id: &str, token: &str) -> Result<Tracking, Erro
     };
     let resp = CLIENT
         .post(&url(&format!("tracking/{}/start", activity_id)))
+        .header("Authorization", auth(token))
         .json(&body)
         .send()
         .await?
         .json::<TrackingResponse>()
         .await?;
     Ok(resp.current_tracking)
+}
+
+async fn stop_tracking(token: &str) -> Result<TimeEntry, Error> {
+    let body = StopTrackingRequest {
+        stopped_at: "2020-08-03T05:00:00.000".to_string(),
+    };
+    let resp = CLIENT
+        .post(&url("/tracking/stop"))
+        .header("Authorization", auth(token))
+        .json(&body)
+        .send()
+        .await?
+        .json::<TimeEntryResponse>()
+        .await?;
+    Ok(resp.created_time_entry)
 }
 
 fn url(path: &str) -> String {
@@ -189,10 +208,38 @@ struct TrackingRequest {
     started_at: String,
 }
 
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct StopTrackingRequest {
+    stopped_at: String,
+}
+
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct TrackingResponse {
     current_tracking: Tracking,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct TimeEntryResponse {
+    created_time_entry: TimeEntry,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct TimeEntry {
+    id: String,
+    activity_id: String,
+    duration: Duration,
+    note: Note,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Duration {
+    started_at: String,
+    stopped_at: String,
 }
 
 #[derive(Deserialize, Debug)]
