e38d8797d638721411b99060005ed8e8db760241
Mario Zupan <mario@zupzup.org>
2020-10-07 12:18:51 +0200

    finish sample

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate lazy_static;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use std::env;
+use tokio::fs;
 
 type Error = Box<dyn std::error::Error>;
 
@@ -11,6 +12,7 @@ lazy_static! {
 }
 
 const BASE_URL: &str = "https://api.timeular.com/api/v3";
+const REPORT_FILE: &str = "./report.csv";
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -27,17 +29,33 @@ async fn main() -> Result<(), Error> {
 
     println!("fetching activities...");
     let activities = fetch_activities(&token).await?;
-    if !activities.is_empty() {
-        let tracking = start_tracking(&activities.get(0).expect("exists").id, &token).await?;
-        print!("started tracking: {:?}", tracking);
-        let time_entry = stop_tracking(&token).await?;
-        print!("created time entry: {:?}", time_entry);
-    }
-    // TODO: create report and write file
-
     println!("activities: {:?}", activities);
 
+    if !activities.is_empty() {
+        println!("starting to track...");
+        let tracking = start_tracking(&activities.get(0).expect("exists").id, &token).await?;
+        println!("started tracking: {:?}", tracking);
+        let time_entry = stop_tracking(&token).await?;
+        println!("created time entry: {:?}", time_entry);
+    }
+
+    println!("creating report...");
+    generate_report(&token).await?;
+    println!("downloaded report to: {}", REPORT_FILE);
+
     Ok(())
+}
+
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct SignInRequest {
+    api_key: String,
+    api_secret: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct SignInResponse {
+    token: String,
 }
 
 async fn sign_in(api_key: String, api_secret: String) -> Result<String, Error> {
@@ -54,90 +72,6 @@ async fn sign_in(api_key: String, api_secret: String) -> Result<String, Error> {
         .await?;
     Ok(resp.token)
 }
-
-async fn fetch_me(token: &str) -> Result<Me, Error> {
-    let resp = CLIENT
-        .get(&url("/me"))
-        .header("Authorization", auth(token))
-        .send()
-        .await?
-        .json::<MeResponse>()
-        .await?;
-    Ok(resp.data)
-}
-
-async fn fetch_spaces(token: &str) -> Result<Vec<Space>, Error> {
-    let resp = CLIENT
-        .get(&url("/space"))
-        .header("Authorization", auth(token))
-        .send()
-        .await?
-        .json::<SpacesResponse>()
-        .await?;
-    Ok(resp.data)
-}
-
-async fn fetch_activities(token: &str) -> Result<Vec<Activity>, Error> {
-    let resp = CLIENT
-        .get(&url("/activities"))
-        .header("Authorization", auth(token))
-        .send()
-        .await?
-        .json::<ActivitiesResponse>()
-        .await?;
-    Ok(resp.activities)
-}
-
-async fn start_tracking(activity_id: &str, token: &str) -> Result<Tracking, Error> {
-    let body = TrackingRequest {
-        started_at: "2020-08-03T04:00:00.000".to_string(),
-    };
-    let resp = CLIENT
-        .post(&url(&format!("tracking/{}/start", activity_id)))
-        .header("Authorization", auth(token))
-        .json(&body)
-        .send()
-        .await?
-        .json::<TrackingResponse>()
-        .await?;
-    Ok(resp.current_tracking)
-}
-
-async fn stop_tracking(token: &str) -> Result<TimeEntry, Error> {
-    let body = StopTrackingRequest {
-        stopped_at: "2020-08-03T05:00:00.000".to_string(),
-    };
-    let resp = CLIENT
-        .post(&url("/tracking/stop"))
-        .header("Authorization", auth(token))
-        .json(&body)
-        .send()
-        .await?
-        .json::<TimeEntryResponse>()
-        .await?;
-    Ok(resp.created_time_entry)
-}
-
-fn url(path: &str) -> String {
-    format!("{}{}", BASE_URL, path)
-}
-
-fn auth(token: &str) -> String {
-    format!("Bearer {}", token)
-}
-
-#[derive(Serialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct SignInRequest {
-    api_key: String,
-    api_secret: String,
-}
-
-#[derive(Deserialize, Debug)]
-struct SignInResponse {
-    token: String,
-}
-
 #[derive(Deserialize, Debug)]
 struct MeResponse {
     data: Me,
@@ -150,6 +84,17 @@ struct Me {
     name: String,
     email: String,
     default_space_id: String,
+}
+
+async fn fetch_me(token: &str) -> Result<Me, Error> {
+    let resp = CLIENT
+        .get(&url("/me"))
+        .header("Authorization", auth(token))
+        .send()
+        .await?
+        .json::<MeResponse>()
+        .await?;
+    Ok(resp.data)
 }
 
 #[derive(Deserialize, Debug)]
@@ -183,6 +128,17 @@ struct RetiredMember {
     name: String,
 }
 
+async fn fetch_spaces(token: &str) -> Result<Vec<Space>, Error> {
+    let resp = CLIENT
+        .get(&url("/space"))
+        .header("Authorization", auth(token))
+        .send()
+        .await?
+        .json::<SpacesResponse>()
+        .await?;
+    Ok(resp.data)
+}
+
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct ActivitiesResponse {
@@ -202,22 +158,75 @@ struct Activity {
     device_side: Option<i64>,
 }
 
+async fn fetch_activities(token: &str) -> Result<Vec<Activity>, Error> {
+    let resp = CLIENT
+        .get(&url("/activities"))
+        .header("Authorization", auth(token))
+        .send()
+        .await?
+        .json::<ActivitiesResponse>()
+        .await?;
+    Ok(resp.activities)
+}
+
 #[derive(Serialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct TrackingRequest {
     started_at: String,
 }
 
-#[derive(Serialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct StopTrackingRequest {
-    stopped_at: String,
-}
-
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct TrackingResponse {
     current_tracking: Tracking,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Tracking {
+    id: i64,
+    activity_id: String,
+    started_at: String,
+    note: Note,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Note {
+    text: Option<String>,
+    tags: Vec<TagOrMention>,
+    mentions: Vec<TagOrMention>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct TagOrMention {
+    id: i64,
+    key: String,
+    label: String,
+    scope: String,
+    space_id: String,
+}
+
+async fn start_tracking(activity_id: &str, token: &str) -> Result<Tracking, Error> {
+    let body = TrackingRequest {
+        started_at: "2020-08-03T04:00:00.000".to_string(),
+    };
+    let resp = CLIENT
+        .post(&url(&format!("/tracking/{}/start", activity_id)))
+        .header("Authorization", auth(token))
+        .json(&body)
+        .send()
+        .await?
+        .json::<TrackingResponse>()
+        .await?;
+    Ok(resp.current_tracking)
+}
+
+#[derive(Serialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct StopTrackingRequest {
+    stopped_at: String,
 }
 
 #[derive(Deserialize, Debug)]
@@ -242,29 +251,39 @@ struct Duration {
     stopped_at: String,
 }
 
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct Tracking {
-    id: String,
-    activity_id: String,
-    started_at: String,
-    note: Note,
+async fn stop_tracking(token: &str) -> Result<TimeEntry, Error> {
+    let body = StopTrackingRequest {
+        stopped_at: "2020-08-03T05:00:00.000".to_string(),
+    };
+    let resp = CLIENT
+        .post(&url("/tracking/stop"))
+        .header("Authorization", auth(token))
+        .json(&body)
+        .send()
+        .await?
+        .json::<TimeEntryResponse>()
+        .await?;
+    Ok(resp.created_time_entry)
 }
 
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct Note {
-    text: Option<String>,
-    tags: Vec<TagOrMention>,
-    mentions: Vec<TagOrMention>,
+async fn generate_report(token: &str) -> Result<(), Error> {
+    let resp = CLIENT
+        .get(&url(
+            "/report/2020-01-01T00:00:00.000/2020-12-31T23:59:59.999?timezone=Europe/Vienna",
+        ))
+        .header("Authorization", auth(token))
+        .send()
+        .await?
+        .bytes()
+        .await?;
+    fs::write(REPORT_FILE, &resp).await?;
+    Ok(())
 }
 
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct TagOrMention {
-    id: i64,
-    key: String,
-    label: String,
-    scope: String,
-    space_id: String,
+fn url(path: &str) -> String {
+    format!("{}{}", BASE_URL, path)
+}
+
+fn auth(token: &str) -> String {
+    format!("Bearer {}", token)
 }
